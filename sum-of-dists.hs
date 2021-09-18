969b3b8937baa0a779a24d47808d1ffe4fcb5abc
Калоян Стоилов <kkstoilov@uni-sofia.bg>
2021-09-18 20:20:49 +0300

    Slightly generalise sum-of-dists

--- sum-of-dists.hs
+++ sum-of-dists.hs
@@ -1,13 +1,13 @@
 import GHC.List (foldl')
 
-scalarProduct :: [Double] -> [Double] -> Double
+scalarProduct :: Floating a => [a] -> [a] -> a
 scalarProduct vec1 vec2 = foldl' (+) 0 $ zipWith (*) vec1 vec2
 
-euclideanNorm :: [Double] -> Double
+euclideanNorm :: Floating a => [a] -> a
 euclideanNorm vec = sqrt $ scalarProduct vec vec
 
-distance :: [Double] -> [Double] -> Double
-distance vec1 vec2 = euclideanNorm $ zipWith (-) vec1 vec2
+euclideanDistance :: Floating a => [a] -> [a] -> a
+euclideanDistance vec1 vec2 = euclideanNorm $ zipWith (-) vec1 vec2
 
 allPairs :: [a] -> [(a, a)]
 allPairs l = allPairs' l [] 
@@ -17,11 +17,13 @@ allPairs l = allPairs' l []
 		allPairs' [x] aggl = aggl
 		allPairs' (x:xs) aggl = allPairs' xs (aggl ++ [(x,y) | y <- xs])
 		
-completeDistance :: [[Double]] -> Double
-completeDistance points = foldl' (+) 0 [distance (fst pair) (snd pair) | pair <- pairs]
+completeDistance :: Floating a => ([a] -> [a] -> a) -> [[a]] -> a
+completeDistance distance points = foldl' (+) 0 [distance (fst pair) (snd pair) | pair <- pairs]
 	where
 		pairs = allPairs points
-		
+
+completeEuclideanDistance = completeDistance euclideanDistance
+
 --Overall distance of sqrt 3
 exampleList = [[0.0,0.0,0.0], [1.0,1.0,1.0]]
 
