605a69b902fc5b69c516ded5b243d3e851f220cb
Калоян Стоилов <kkstoilov@uni-sofia.bg>
2021-11-08 22:38:05 +0200

    Add binary

--- merge.hs
+++ merge.hs
@@ -1,6 +1,6 @@
 merge::(Ord a)=>[a]->[a]->[a]
-merge [] xs =xs
-merge xs [] =xs
+merge [] xs = xs
+merge xs [] = xs
 merge (x:xs) (y:ys) =if(x<=y) then [x]++merge xs (y:ys) else [y]++merge (x:xs) ys
 
 first_half :: [a] -> [a]
@@ -14,6 +14,6 @@ second_half = (\xs -> case xs of
             xs -> drop ((length xs) `div` 2 ) xs)
 
 mergeSort::(Ord a)=>[a]->[a]
-mergeSort [] =[]
-mergeSort [x] =[x]
-mergeSort xs =merge (mergeSort (first_half xs) ) (mergeSort (second_half xs) )
+mergeSort []  = []
+mergeSort [x] = [x]
+mergeSort xs  = merge (mergeSort (first_half xs) ) (mergeSort (second_half xs) )
