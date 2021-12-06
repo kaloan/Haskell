0a31fd560571369cad37db69db811c410a2996a0
Калоян Стоилов <kkstoilov@uni-sofia.bg>
2021-12-07 00:20:45 +0200

    More efficient power series

--- powerseries.hs
+++ powerseries.hs
@@ -1,12 +1,15 @@
 import           Data.List (foldl')
 
-iterateWithIndex :: (Eq b, Enum b) => (a -> b -> a) -> (a -> b -> a) -> a -> [a]
+iterateWithIndex :: (Eq b, Enum b) => (a -> b -> c) -> (a -> b -> a) -> a -> [c]
 iterateWithIndex = iterateWithIndex' (toEnum 0)
   where
     iterateWithIndex' n f g x = f x n : iterateWithIndex' (succ n) f g (g x n)
 
-powerSeries :: (Eq b, Enum b) => (a -> b -> a) -> b -> a -> [a]
+powerSeries :: (Eq b, Enum b) => (a -> b -> c) -> b -> a -> [c]
 powerSeries f n = take (fromEnum n) . iterateWithIndex f const
+
+powerSeries' :: (Eq b, Enum b) => (a -> b -> c) -> (a -> b -> a) -> b -> a -> [c]
+powerSeries' f g n = take (fromEnum n) . iterateWithIndex f g
 
 factorial :: Integer -> Integer
 factorial n = foldl' (*) 1 [1 .. n]
@@ -20,18 +23,18 @@ stRound :: Double -> Double
 stRound = round' 15
 
 sine :: Double -> Double
-sine x = stRound $ x * sum (powerSeries go 40 x)
+sine x = stRound $ sum $ powerSeries' go go' 40 (x, 1)
   where
-    go :: Double -> Integer -> Double
-    --go x n = f $ (x ^ (2 * n)) / fromIntegral ((2 * n + 1) * (2 * (n + 1)))
-    go x n = f $ (x ^ (2 * n)) / fromIntegral (factorial (2 * n + 1))
-      where
-        f = if even n then id else negate
+    go :: (Double, Double) -> Integer -> Double
+    go (y, acc) n = y / acc
+    go' :: (Double, Double) -> Integer -> (Double, Double)
+    go' (y, acc) n = (negate $ x * x * y, acc * fromIntegral ((2 * n + 3) * (2 * n + 2)))
 
 cosine :: Double -> Double
-cosine x = stRound $ sum $ powerSeries go 40 x
+--cosine x = stRound $ sum $ powerSeries go 40 x
+cosine x = stRound $ sum $ powerSeries' go go' 40 (1, 1)
   where
-    go :: Double -> Integer -> Double
-    go x n = f $ (x ^ (2 * n)) / fromIntegral (factorial (2 * n))
-      where
-        f = if even n then id else negate
+    go :: (Double, Double) -> Integer -> Double
+    go (y, acc) n = y / acc
+    go' :: (Double, Double) -> Integer -> (Double, Double)
+    go' (y, acc) n = (negate $ x * x * y, acc * fromIntegral ((2 * n + 2) * (2 * n + 1)))
