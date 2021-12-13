82690edf38998295e54df36a0a49937b30d38feb
Калоян Стоилов <kkstoilov@uni-sofia.bg>
2021-12-13 09:06:31 +0200

    Solve advent of code 13

--- advent-of-code/day13/day13.hs
+++ advent-of-code/day13/day13.hs
@@ -11,11 +11,34 @@
 {-# OPTIONS_GHC -fwarn-unused-matches #-}
 
 import           Control.Monad
-import           Data.List     (foldl', sort)
-import           Data.Strings  (strSplit, strSplitAll)
+import           Data.List        (foldl', sort)
+import           Data.List.Unique
+import           Data.Matrix
+import           Data.Strings     (strSplit, strSplitAll)
 import           System.IO
 
 -- 13.12.21
+foldAlong :: (Num a, Ord a) => ((a, a) -> a) -> ((a, a) -> a -> (a, a)) -> a -> [(a, a)] -> [(a, a)]
+foldAlong f g about = foldl' go []
+  where
+    go acc x
+      | around > about = g x (about + about - around) : acc
+      | around < about = g x around : acc
+      | otherwise = acc
+      where
+        around = f x
+
+foldTrue :: (Num a, Ord a) => (Char, a) -> [(a, a)] -> [(a, a)]
+foldTrue ('x', line) = uniq . sort . foldAlong fst flipX line
+foldTrue ('y', line) = uniq . sort . foldAlong snd flipY line
+foldTrue _           = undefined
+
+flipX :: (a, b) -> a -> (a, b)
+flipX (_, y) newX = (newX, y)
+
+flipY :: (a, b) -> b -> (a, b)
+flipY (x, _) newY = (x, newY)
+
 mapPair :: (a -> c) -> (b -> d) -> (a, b) -> (c, d)
 mapPair f g (x, y) = (f x, g y)
 
@@ -34,9 +57,13 @@ mainWork filename = do
   let (points', folds') = strSplit "\n\n" contents
   let points = map (mapPair readInt readInt . strSplit ",") $ lines points'
   let folds = map (mapPair head readInt . strSplit "=" . (head . tail . tail . words)) (lines folds')
-  print points
-  print folds
+  print $ length $ foldTrue (head folds) points
+  let endCoords = foldl' (\acc dir -> foldTrue dir acc) points folds
+  let cols = succ $ maximum $ map fst endCoords
+  let rows = succ $ maximum $ map snd endCoords
+  let matrixStart = zero rows cols
+  print $ foldl' (\acc (x, y) -> setElem 1 (succ y, succ x) acc) matrixStart endCoords
 
 main :: IO ()
 main =
-  mainWork "test.txt"
+  mainWork "input.txt"
